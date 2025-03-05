6698e816873b7930bfa97c5dad32df637188ed37
perpetual-cosmos <prataptarunsingh241@gmail.com>
2025-03-06 00:54:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Log when the hero button is clicked.
+document.addEventListener('DOMContentLoaded', () => {
+    const heroBtn = document.querySelector('.hero-btn');
+    heroBtn.addEventListener('click', () => {
+      console.log('User clicked to join the WhatsApp group.');
+    });
+  });
+  
